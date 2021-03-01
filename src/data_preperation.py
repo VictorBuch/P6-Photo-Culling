fb689e3b6881a6014a2229719a0ec129ab8fcdd0
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2021-03-01 10:42:31 +0100

    Refactoring data prep with data-generator.

--- src/data_preperation.py
+++ src/data_preperation.py
@@ -10,16 +10,17 @@ import pandas as pd
 
 # Whatever preprocessing necessary (maybe cropping, reshaping) to get the dataset into training material
 
-IMAGE_DATASET_PATH = "../datasets/images_subset/"
-TEXT_FILE_PATH = "../datasets/AVA.txt"
+IMAGE_DATASET_PATH = "../datasets/images"
+IMAGE_DATASET_SUBSET_PATH = "../datasets/images_subset/"
+IMAGE_INFO_PATH = "../datasets/AVA.txt"
 
 
-def prepare_dataset(image_dataset_path, text_file_path):
+def prepare_dataset(image_dataset_path, image_info_path):
     # This method's task is to get the image data from the folder into numpy arrays
     # and also to dissect the AVA.txt's content and extract the ranks into numpy array of labels (and associate them
     # with the images they correspond to.
 
-    dataframe = pd.read_csv(text_file_path, sep=' ')
+    dataframe = pd.read_csv(image_info_path, sep=' ')
 
     images = []
     labels = []
@@ -56,11 +57,45 @@ def prepare_dataset(image_dataset_path, text_file_path):
     return images, labels
 
 
+def get_labels(image_dataset_path, image_info_path):
+    dataframe = pd.read_csv(image_info_path, sep=' ')
+
+    labels = []
+
+    count = 0
+
+    for filename in os.listdir(image_dataset_path):
+
+        # Get the row from AVA.txt dataframe where the index (in dataframe) equals filename minus .jpg.
+        image_data = dataframe[dataframe["index"] == int(filename.split('.')[0])]
+
+        if image_data is None:
+            # We will remove these from the directory, but first I wanna see how many (if any) we're talking.
+            count += 1
+            continue
+
+        # Get the average of ranks (although literature suggests to use different metrics).
+        num_annotations = 0
+        rank_sum = 0
+
+        for i in range(1, 11):
+            rank_sum += image_data[str(i)] * i
+            num_annotations += image_data[str(i)]
+
+        average_rank = rank_sum / num_annotations
+        labels.append(average_rank)
+
+    print("This many fucked up: {}".format(count))
+    return labels
+
+
 if __name__ == "__main__":
-    X, y = prepare_dataset(IMAGE_DATASET_PATH, TEXT_FILE_PATH)
+    # X, y = prepare_dataset(IMAGE_DATASET_SUBSET_PATH, IMAGE_INFO_PATH)
 
-    print("Input shape: {}.".format(X.shape))
-    print("Output shape: {}.".format(y.shape))
+    # print("Input shape: {}.".format(X.shape))
+    # print("Output shape: {}.".format(y.shape))
 
-    # This takes ~8 seconds with just 210 examples. We might need to find a better way to load 32GB of data
-    # into 16GM of RAM later on.
+    labels = get_labels(IMAGE_DATASET_PATH, IMAGE_INFO_PATH)
+
+    print("This many labels: {}".format(len(labels)))
+
--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from src.image_ranking_service import *
 MODEL_PATH = "models/model.h5"
 
 if __name__ == "__main__":
-    irs = ImageRankingService(MODEL_PATH)
+    (MODEL_PATH)

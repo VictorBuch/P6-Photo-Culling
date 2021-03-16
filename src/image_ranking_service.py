02514f71533ddbec94145546c81cda82b43d2059
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2021-03-16 11:21:21 +0100

    GIAA_mean refactor.

--- src/image_ranking_service.py
+++ src/image_ranking_service.py
@@ -1,4 +1,9 @@
+"""
+Using the trained model(s) to process a new image, or a set of images.
+"""
+
 import tensorflow.keras as keras
+import numpy as np
 
 
 class ImageRankingService:
@@ -7,10 +12,8 @@ class ImageRankingService:
         self.model = keras.models.load_model(model_path)
 
     def predict(self, image):
-        # 1, Preprocess image the same way it was preprocessed for training.
-        # 2, Predict ...
 
-        return self.model.predict(image)
+        return self.model.predict(image[np.newaxis, ...])
 
 
 

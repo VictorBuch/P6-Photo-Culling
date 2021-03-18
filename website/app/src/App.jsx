a8d73476541f6a7af403ddac2156250795ec9de0
Victor Buch <vbuch18@student.aau.dk>
2021-03-18 18:59:59 +0100

    Dynamic Images with React

--- website/app/src/App.jsx
+++ website/app/src/App.jsx
@@ -1,22 +1,63 @@
-// This is the file where we biuld our app :)
+import React, { useState } from "react";
 
-import React from "react";
-import ImageCardDeck from "./Components/ImageCardDeck";
-import RankedImage from "./Components/RankedImage";
+export default function App() {
+  const imageFileArr = [];
+  const imageBlobs = [];
+  const [imageArr, setImageArr] = useState([]);
 
-function App() {
+  function loadImages(e) {
+    imageFileArr.push(e.target.files); // gets a file object with all files
+    console.log(imageFileArr[0]); // this gives an image file
+    // console.log("File arr length: " + imageFileArr[0].length);
+
+    // Loop trough all the local images and creat blob elements for later use
+    for (let i = 0; i < imageFileArr[0].length; i++) {
+      imageBlobs.push(URL.createObjectURL(imageFileArr[0][i]));
+    }
+    setImageArr(imageBlobs); // set the dynamic state array equal to the blobs we just made
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-      }}
-    >
-      <ImageCardDeck />
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col md-12">
+          <h1 style={{ color: "white" }}>Test area</h1>
+
+          <label className="btn btn-danger">
+            <div>Upload Images</div>
+            <input
+              id="inputFile"
+              className="file-upload"
+              type="file"
+              accept="image/*"
+              onChange={loadImages}
+              multiple
+            />
+          </label>
+
+          {/* This section will need to be a JSX comonent soon but for now it dynamically loads the images */}
+          <div className="Images">
+            {imageArr.map((index) => {
+              return (
+                <img key={index} src={index} alt="" className="uploadedImg" />
+              );
+            })}
+          </div>
+
+          {/* <input
+            style={{ display: "block" }}
+            className="btn btn-danger"
+            onChange={loadImages}
+            placeholder="Upload Images"
+            type="file"
+          /> */}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+// the tasks
+// have a button that accepts only PNG's and JPG's --------V---------
+// save those images in an array -----------V------------
+// use that array of images to dynamically display images, propably using state

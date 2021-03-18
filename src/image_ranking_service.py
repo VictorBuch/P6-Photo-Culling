69875a008611f9b84fffd17107624225f5c7d673
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2021-03-18 13:04:07 +0100

    Saved a model in JS readable format.

--- src/image_ranking_service.py
+++ src/image_ranking_service.py
@@ -2,14 +2,14 @@
 Using the trained model(s) to process a new image, or a set of images.
 """
 
-import tensorflow.keras as keras
+import tensorflow as tf
 import numpy as np
 
 
 class ImageRankingService:
 
     def __init__(self, model_path):
-        self.model = keras.models.load_model(model_path)
+        self.model = tf.keras.models.load_model(model_path)
 
     def predict(self, image):
 

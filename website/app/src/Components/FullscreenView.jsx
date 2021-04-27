87304e8e3c011801042919c1554ff30c5ec7461d
Victor Buch <47211196+VictorBuch@users.noreply.github.com>
2021-04-27 11:30:49 +0200

    FULLSCREEN WORKS

---
+++ website/app/src/Components/FullscreenView.jsx
@@ -0,0 +1,73 @@
+import { useContext, useEffect, useState } from "react";
+
+// components
+import VerticalCluster from "./VerticalCluster";
+import Cluster from "./Cluster";
+import { NavContext } from "./NavContext";
+
+// styles
+import styled from "styled-components";
+
+export default function FullscreenView() {
+  //states
+
+  const { globalyStoredClusters, globalOrange } = useContext(NavContext);
+  const [storedClusters, setStoredClusters] = globalyStoredClusters;
+  const [orange, setOrange] = globalOrange;
+  const [clusterIndex, setClusterIndex] = useState(0);
+
+  function changeOffset(direction) {
+    if (clusterIndex + direction >= storedClusters.length - 1) {
+      return setClusterIndex(0);
+    }
+    if (clusterIndex + direction <= 0) {
+      return setClusterIndex(storedClusters.length - 1);
+    }
+    console.log("Got to change offset bottom");
+    setClusterIndex((prev) => (prev += direction));
+  }
+
+  useEffect(() => {
+    setClusterIndex(
+      storedClusters.findIndex((element) => element.includes(orange))
+    );
+  }, []);
+
+  return (
+    <StyledFullscreenSection
+      className="container-fluid m-2"
+      id="fullscreenView"
+    >
+      <img
+        className=""
+        src={
+          storedClusters[clusterIndex].includes(orange)
+            ? orange
+            : storedClusters[clusterIndex][0]
+        }
+        alt=""
+      />
+
+      <VerticalCluster index={clusterIndex} setOffset={changeOffset} />
+
+      <Cluster
+        imageBlobArr={storedClusters[clusterIndex]}
+        isFullScreen={true}
+      />
+      <h1>Info and shit</h1>
+    </StyledFullscreenSection>
+  );
+}
+
+const StyledFullscreenSection = styled.section`
+  height: 90vh;
+  display: grid;
+  grid-template-columns: 2.5fr 1fr;
+  grid-template-rows: 2.5fr 1fr;
+  gap: 1em;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+`;

39e9ddcfff61c48070faa2b81fbc84d7e0210a54
Victor Buch <vbuch18@student.aau.dk>
2021-05-03 11:28:59 +0200

    Update CullingView.jsx

--- website/app/src/Components/CullingView.jsx
+++ website/app/src/Components/CullingView.jsx
@@ -10,8 +10,11 @@ import styled from "styled-components";
 
 export default function CullingView({ imageBlobArr }) {
   const [isFullScreen, setIsFullScreen] = useState(false);
-  const { globalSelectedImageKey } = useContext(NavContext);
+  const { globalSelectedImageKey, globalAcceptedImages } = useContext(
+    NavContext
+  );
   const [selectedImageKey, setSelectedImageKey] = globalSelectedImageKey;
+  const [acceptedImageKeys, setAcceptedImageKeys] = globalAcceptedImages;
 
   document.addEventListener("keydown", handleKeyDown);
 
@@ -27,12 +30,50 @@ export default function CullingView({ imageBlobArr }) {
         setIsFullScreen(false);
         document.removeEventListener("keydown", handleKeyDown);
         break;
-      case "p":
-        console.log();
+      case "s":
+        console.log("Firing S");
+        download(acceptedImageKeys);
         break;
       default:
         break;
     }
+  }
+
+  function download(blobArray) {
+    const a = document.createElement("a");
+    a.href = blobArray[0];
+    a.download = "myImage.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  function downloadBlob(blobArray, name = "file.png") {
+    console.log(blobArray);
+    const blobUrl = blobArray[1];
+
+    // Create a link element
+    const link = document.createElement("a");
+
+    // Set link's href to point to the Blob URL
+    link.href = blobUrl;
+    link.download = name;
+
+    // Append link to the body
+    document.body.appendChild(link);
+
+    // Dispatch click event on the link
+    // This is necessary as link.click() does not work on the latest firefox
+    // link.dispatchEvent(
+    //   new MouseEvent("click", {
+    //     bubbles: true,
+    //     cancelable: true,
+    //     view: window,
+    //   })
+    // );
+    link.click();
+    // Remove link from body
+    document.body.removeChild(link);
   }
 
   const netflix = (
